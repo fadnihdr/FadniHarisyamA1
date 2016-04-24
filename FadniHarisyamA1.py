1c386bf4fc188c03afdb715b54a668d8265f36e4
fadnihdr <fadnihdr@gmail.com>
2016-04-25 00:24:40 +0800

    added 2 menu options

--- FadniHarisyamA1.py
+++ FadniHarisyamA1.py
@@ -2,11 +2,34 @@
 for docstring
 '''
 
+print("Items for Hire ")
+print("{} items loaded")
+
+menu = ("Menu:\n (L)ist all items\n (H)ire an item\n (R)eturn an item\n (A)dd new item to stock\n (Q)uit")
 openItem = open('items.csv', 'r')
 readItem = openItem.readlines()
+userInput = input(menu).lower()
 
-for i in readItem:
-    name, item_desc, cost, status = i.split(",")
-    print(name, item_desc, cost, status)
+
+
+while True:
+    if userInput == "q":
+        print ("{} items saved to items.csv")
+        break
+    elif userInput == "l":
+        for words in readItem:
+            name, item_desc, cost, status = words.split(",")
+        print(readItem)
+        userInput = input(menu).lower()
+
+
+
+
+
+
+
+
+
+
 
 
